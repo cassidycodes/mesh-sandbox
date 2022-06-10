cd144175967bd4c4934be85482a34273d7bf0780
Cassidy Scheffer <hello@cassidy.codes>
2022-06-10 09:05:00 -0400

    example with blank context

---
+++ src/example-backend/app.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var { graphqlHTTP } = require("express-graphql");
+var { buildSchema } = require("graphql");
+
+// Construct a schema, using GraphQL schema language
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return "Hello world!";
+  },
+};
+
+var app = express();
+app.use((req, res, next) => {
+  console.log(req.headers);
+  next();
+});
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
+app.listen(3002);
+console.log("Running a GraphQL API server at http://localhost:3002/graphql");
